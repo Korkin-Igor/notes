a785aa58305b0133e7b5cc3df3eafc3707da7ca3
Korkin-Igor <korkinigorttit@gmail.com>
2026-03-10 14:58:32 +0700

    Set up using custom title, Create button "add-note-button"

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,12 +1,34 @@
 <script setup>
-
+  defineProps({
+    title: String,
+  })
 </script>
 
 <template>
-<div>
-</div>
+  <div class="note-card">
+    <h2>{{ title }}</h2>
+    <button class="add-note-button" v-if="title === 'New'">Add note</button>
+  </div>
 </template>
 
 <style scoped>
+.note-card {
+  background: #ccc;
+
+  padding: 10px;
+
+  width: 30vw;
+  height: 3vw;
+  min-width: 200px;
+  min-height: 70px;
+}
+
+.add-note-button {
+  width: 40%;
+  height: 40%;
+
+  position: relative;
+  left: -60px;
+}
 
 </style>
